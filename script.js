9fa8484162dcb706b00fe11c04933f459014bd48
Jake Browning <jacobbrowning90@gmail.com>
2022-11-15 10:42:18 +0000

    Changed toggle button to display readStatus of parent Book

--- script.js
+++ script.js
@@ -34,45 +34,45 @@ function addBookToLibrary(Book) {
 //Call new book form
 callFormButton.addEventListener('click', function(){
     callFormButton.remove();
-    var linebreak = document.createElement("br");
-    var titleLabel = document.createElement("label");
+    const linebreak = document.createElement("br");
+    const titleLabel = document.createElement("label");
     titleLabel.setAttribute("for", "title");
     titleLabel.textContent = "Title:";
-    var titleField = document.createElement("input");
+    const titleField = document.createElement("input");
     titleField.setAttribute("required", "");
     titleField.setAttribute("id", "title");
     titleField.setAttribute("name", "title");
-    var authorLabel = document.createElement("label");
+    const authorLabel = document.createElement("label");
     authorLabel.textContent = "Author:";
-    var authorField = document.createElement("input");
+    const authorField = document.createElement("input");
     authorField.setAttribute("required", "");
     authorField.setAttribute("id", "author");
     authorField.setAttribute("name", "author");
-    var pagesLabel = document.createElement("label");
+    const pagesLabel = document.createElement("label");
     pagesLabel.textContent = "Page count:";
-    var pagesField = document.createElement("input");
+    const pagesField = document.createElement("input");
     pagesField.setAttribute("required", "");
     pagesField.setAttribute("id", "pages");
     pagesField.setAttribute("name", "pages");
-    var readLabel = document.createElement("label");
+    const readLabel = document.createElement("label");
     readLabel.textContent = "Read:";
     readLabel.setAttribute("for", "readTrue")
-    var readRadio = document.createElement("input");
+    const readRadio = document.createElement("input");
     readRadio.setAttribute("type", "radio");
     readRadio.setAttribute("id", "readTrue");
     readRadio.setAttribute("name", "readStatus");
     readRadio.setAttribute("checked", "");
-    var unreadLabel = document.createElement("label");
+    const unreadLabel = document.createElement("label");
     unreadLabel.textContent = "Unread:";
     unreadLabel.setAttribute("for", "readFalse")
-    var unreadRadio = document.createElement("input");
+    const unreadRadio = document.createElement("input");
     unreadRadio.setAttribute("type", "radio");
     unreadRadio.setAttribute("id", "readFalse");
     unreadRadio.setAttribute("name", "readStatus");
-    var submitButton = document.createElement("input");
+    const submitButton = document.createElement("input");
     submitButton.setAttribute("type", "submit");
     submitButton.setAttribute("id", "addBook");
-    var cancelButton = document.createElement("input");
+    const cancelButton = document.createElement("input");
     cancelButton.setAttribute("type", "button");
     cancelButton.setAttribute("id", "cancelButton");
     cancelButton.setAttribute("value", "Cancel");
@@ -100,7 +100,6 @@ callFormButton.addEventListener('click', function(){
     bookForm.appendChild(linebreak);
     bookForm.appendChild(submitButton);
     bookForm.appendChild(cancelButton);
-
 });
 
 //Get new book values from form, then add to library and print library
@@ -112,7 +111,7 @@ bookForm.addEventListener('submit', (event) => {
     } else {
         readStatus = false;
     }
-    var newBook = new Book(title.value, author.value, pages.value, readStatus);
+    const newBook = new Book(title.value, author.value, pages.value, readStatus);
     addBookToLibrary(newBook);
     printLibrary();
   });
@@ -128,7 +127,12 @@ function printLibrary() {
 
         let readToggle = document.createElement('button');
         readToggle.classList.add('readToggle');
-        readToggle.textContent = "Update read / unread";
+        if (library[entry].readStatus == true) {
+            readToggle.textContent = "Finished";
+        } else if (library[entry].readStatus == false) {
+            readToggle.textContent = "Unfinished";
+        }
+
         readToggle.addEventListener("click", function () {
             library[entry].changeReadStatus();
         });
